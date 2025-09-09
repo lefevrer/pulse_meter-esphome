c5633bed60fad40b0c01df4f6de2264713ab7b92
lefevrer <lefevreremy@gmail.com>
2025-09-09 20:37:17 +0200

    Update pulse_meter_sensor.cpp

--- esphome/components/pulse_meter/pulse_meter_sensor.cpp
+++ esphome/components/pulse_meter/pulse_meter_sensor.cpp
@@ -57,7 +57,9 @@ void PulseMeterSensor::loop() {
     this->last_pin_val_ = current;
 
     // Copy set into get to get the latest state from the ISR and reset the count in set
-    *this->get_ = *this->set_;
+    this->get_->last_detected_edge_us_ = this->set_->last_detected_edge_us_;
+    this->get_->last_rising_edge_us_ = this->set_->last_rising_edge_us_;
+    this->get_->count_ = this->set_->count_;
     this->set_->count_ = 0;
   }
 
